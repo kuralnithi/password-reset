25f5ed07627d21ec53ae84056563533f095d010f
kuranithi <kuralnithi1999@gmail.com>
2023-10-10 13:58:33 +0530

    login frontend commit1

---
+++ src/Components/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import Home from '../Home';
+import ResetPasswordpage from './ResetPasswordpage';
+import { createContext } from 'react';
+
+function App (props) {
+  
+  
+    return (
+        <div>
+
+            <Routes>
+
+            <Route path='/' element={<Home />} />
+            <Route path='/Preset' element={<ResetPasswordpage />} />
+
+
+            </Routes>
+
+
+
+
+
+        </div>
+    );
+}
+
+export default App;
+
+
